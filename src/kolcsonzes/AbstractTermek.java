ebaf39e8a90dcab520ccdcba312eca2b7258402d
Petrovai Asztrik Örs <asztrikpetrovai@gmail.com>
2023-12-06 13:02:58 +0100

    abstract

--- src/kolcsonzes/AbstractTermek.java
+++ src/kolcsonzes/AbstractTermek.java
@@ -1,5 +1,24 @@
 package kolcsonzes;
 
-public class AbstractTermek {
+import java.util.UUID;
     
+public abstract class AbstractTermek {
+   private UUID id;
+   private String nev;
+   private int ar;
+
+    public AbstractTermek(String nev, int ar) {
+        this.nev = nev;
+        this.ar = ar;
+    }
+   
+   public void idgenerator(){
+       this.id = UUID.randomUUID();
+   }
+
+    @Override
+    public String toString() {
+        return "AbstractTermek{" + "id=" + id + ", nev=" + nev + ", ar=" + ar + '}';
+    } 
+   
 }
