9de8e358d834a4fcc9178dd211935b124dd1493f
Warhair <tegzesmarton@gmail.com>
2023-12-06 13:36:33 +0100

    Konyvtarszerkezet_szerkesztese

--- src/kolcsonzes/Konyvtar.java
+++ src/kolcsonzes/Konyvtar.java
@@ -1,5 +1,24 @@
 package kolcsonzes;
 
 public class Konyvtar {
+    private Konyvtar[] konyv = new Konyvtar[2];
+
+    public Konyvtar(){
+        felvesz();
+    }
     
+    
+    public void kolcsonzheto(){
+        for (int i = 0; i < konyv.length; i++) {
+           if(konyv[i].isAllapot){
+               System.out.println(konyv[i].toString());
+           }
+        }
+    }
+    public void felvesz(){
+       konyv[0] += new ErtekesKonyv("gomboc", 1500);
+       konyv[1] += new OlcsoKonyv("korte", 1000, true);
+
 }
+}
+
